817eb50f2406c55d69dff480eaeed20e01e6d789
Olga Filchukova <makeeva.o.y@gmail.com>
2024-03-31 21:14:50 +0300

    fix check before payment component

--- src/component/CheckBox/CheckBox.jsx
+++ src/component/CheckBox/CheckBox.jsx
@@ -1,12 +1,17 @@
-import './CheckBox.css';
+import './CheckBox.css'
+import { useLocation } from 'react-router-dom'
 
 function CheckBox(props) {
+
+  let { pathname } = useLocation();
 
   return (
     <div className='checkbox'>
       <label className='checkbox__label'>
         <input className='checkbox__input' type="checkbox" name={props.checkboxName} id={props.checkboxValue} value={props.checkboxValue} onChange={props.onChange} checked={props.checked} />
-        <span className='checkbox__custom'></span>
+        {pathname !== '/HRspace/formpayment'
+          ? <span className='checkbox__custom'></span>
+          : <span className='checkbox__custom checkbox__custom_lower'></span>}
         {props.checkboxTitle}
       </label>
     </div>
--- src/component/StepCheckBeforePayment/StepCheckBeforePayment.jsx
+++ src/component/StepCheckBeforePayment/StepCheckBeforePayment.jsx
@@ -1,10 +1,11 @@
 import './StepCheckBeforePayment.css'
-import '../Section/Section.css'
+// import '../Section/Section.css'
 import '../Link/Link.css'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import Error from '../Error/Error.jsx'
 import CurrentForm from '../CurrentForm/CurrentForm.jsx'
+import CheckBox from '../CheckBox/CheckBox.jsx'
+import Error from '../Error/Error.jsx'
 import Button from '../Button/Button.jsx'
 
 
@@ -18,25 +19,42 @@ function StepCheckBeforePayment() {
   const onRedirect = () => navigate('/HRspace/formsubmit');
 
   return (
-    <div className='payment-check section'>
+    <div className='payment-check'>
+      <div className='payment-check__container'>
+        <h2 className='payment-check__title'>Проверь заявку для публикации</h2>
 
-      <div className='payment-check__error'>
-        <Error errorMessage={errorMessage} />
-      </div>
+        <div className='payment-check__current-form'>
+          <CurrentForm />
+        </div>
 
-      <div className='payment-check__current-form section__currentform-position'>
-        <CurrentForm />
-      </div>
+        <div className='payment-check__checkbox'>
+          <CheckBox
+            checkboxTitle='Я принимаю условия «Оферты на заключение договора об использовании
+      веб-сервиса HRspace»'
+            checkboxValue='agree'
+            checkboxName='oferta'//от бэка
+          />
+        </div>
 
-      <div className='payment-check__button section__button-position'>
-        <Button
-          onClick={onRedirect}
-          stepIsValid={stepIsValid}
-          buttonTitle='Далее'
-          buttonType='button'
-        />
+        <div className='payment-check__error'>
+          <Error errorMessage={errorMessage} />
+        </div>
+
+        <div className='payment-check__button'>
+          <Button
+            stepIsValid='true'
+            buttonTitle='Сохранить черновик'
+            buttonType='button'
+          />
+          <Button
+            onClick={onRedirect}
+            stepIsValid={stepIsValid}
+            buttonTitle='Далее'
+            buttonType='button'
+          />
+        </div>
       </div>
-    </div>
+    </div >
   )
 }
 
